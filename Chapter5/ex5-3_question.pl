f64d8ca8c7193517cab142ffa1fa69a22cf7f7ba
Wei Wang <onevcat@gmail.com>
2012-06-08 01:49:26 +0800

    Add question for ex5-3.
    Signed-off-by: Wei Wang <onevcat@gmail.com>

---
+++ Chapter5/ex5-3_question.pl
@@ -0,0 +1,15 @@
+#! /usr/bin/perl
+use strict;
+
+print "Enter a position to align strings: ";
+chomp (my $position = <STDIN>);
+print "Enter some strings. I will align them to right with $position-characters column:\n";
+
+#chomp is important here, otherwise we will get a ($position - 1)-column alignment.
+chomp (my @list = <STDIN>);
+
+print "1234567890" x ($position / 10), (1 .. $position % 10), "\n";
+
+foreach (@list) {
+	printf "%${position}s\n", $_;
+}
