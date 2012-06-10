b7199e12677db2612c5a17d8d1e2cdbaa173dabf
Wei Wang <onevcat@gmail.com>
2012-06-10 22:59:07 +0800

    Chapter8.pdf added.
    Signed-off-by: Wei Wang <onevcat@gmail.com>

--- Chapter8/ex8-5.pl
+++ Chapter8/ex8-5.pl
@@ -3,8 +3,11 @@ use strict;
 
 while (<>) { # take one input line at a time
 	chomp;
-	if (/(?<word>\w*a\b(?<append>.{0,5}))/) {
-		print "Matched: |$`<$&>$'|. \'word\' contains: \'$+{word}\'. \'append\' contains: \'$+{append}\'.\n"; # the special match vars
+	if (/((?<word>\w*a\b)(?<append>.{0,5}))/) {
+		print "Matched: |$`<$&>$'|. \'word\' contains: \'$+{word}\'\n"; # the special match vars
+		if ($+{append}) {
+			print "\'append\' contains: \'$+{append}\'.\n";
+		}
 	} else {
 		print "No match: |$_|\n"; }
 }
